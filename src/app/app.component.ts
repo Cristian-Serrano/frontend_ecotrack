7178e22421fd8eac6a1330cae71e9bb3f2719468
cristian <ejemplo@ejemplo.com>
2024-03-20 14:49:24 +0100

    estilos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarSuperiorComponent } from './componentes/navbar-superior/navbar-superior.component';
+import { NavbarInferiorComponent } from './componentes/navbar-inferior/navbar-inferior.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarSuperiorComponent],
+  imports: [RouterOutlet,NavbarSuperiorComponent, NavbarInferiorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
