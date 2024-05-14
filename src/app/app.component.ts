904507d7594b0164c5600f7ef16e0c5de1bb7551
cristian <ejemplo@ejemplo.com>
2024-05-14 20:46:38 +0200

    añadido footer y main

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarSuperiorComponent } from './componentes/navbar-superior/navbar-superior.component';
 import { NavbarInferiorComponent } from './componentes/navbar-inferior/navbar-inferior.component';
+import { MainComponent } from './componentes/main/main.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavbarSuperiorComponent, NavbarInferiorComponent],
+  imports: [RouterOutlet,NavbarSuperiorComponent, NavbarInferiorComponent, MainComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.sass'
 })
